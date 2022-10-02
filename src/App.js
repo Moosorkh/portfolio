e24d44dd651c919bd3c2154e43ea9686bd08a5b4
Fortis Irdmousa <moosorkh@gmail.com>
2022-10-02 02:52:14 -0700

    portfolio pre-completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Particles from 'react-particles-js';
+
 
 function App() {
   return (
     <>
+    {/* <Particles /> */}
       <Navbar />,
       <Header />
     </>
